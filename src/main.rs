8453d810cdf2a204399fd9702b99ff4edf96ecb8
bhaweshpanwar <panwarbhawesh1112@gmail.com>
2026-01-04 00:21:29 +0530

    Add initial project setup with dependencies and main application structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tokio::join;
+
+mod routes;
+mod db;
+mod cron;
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+
+    let pool = db::connect_db().await;
+
+    let app = routes::create_routes();
+
+    let server = async {
+        axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    };
+
+    let cron = async {
+        cron::scheduler::start_scheduler(pool).await;
+    };
+
+    join!(server, crom);
 }
